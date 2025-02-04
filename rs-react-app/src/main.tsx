dae6c401ea7b3140082de289180aa3738fe01850
MaksRybolt <102859892+Max071298@users.noreply.github.com>
2025-02-04 22:07:43 +0300

    fix: remove unnecessary rs-react-app folder

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import ErrorBoundary from './Components/ErrorBoundary.tsx';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
